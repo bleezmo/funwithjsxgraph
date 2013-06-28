79d8643b94b9d90190ae8b9f6865b5623fc4beaf
bleezmo <joshuarogers10@gmail.com>
2013-06-28 15:23:52 -0400

    added y=mx+b equation. added values text for points. display current x and y values

--- public/javascripts/Canvas.js
+++ public/javascripts/Canvas.js
@@ -1,9 +1,12 @@
 angular.module('jsxgraph', []).factory('Canvas', function(){
 	return function(board){
 		this.points = [];
+		//given mouse clicke event e, get the coordinates on the board that was clicked
 		this.getMouseCoords = function(e){
 	        return new JXG.Coords(JXG.COORDS_BY_SCREEN, [e.offsetX, e.offsetY], board);
 		}
+		//if there is a collision, return the point that coords collides with
+		//otherwise return undefined
 		this.pointCollision = function(coords){
 			for (var el in board.objects) {
 	            if(JXG.isPoint(board.objects[el]) && board.objects[el].hasPoint(coords.scrCoords[1], coords.scrCoords[2])) {
@@ -12,24 +15,33 @@ angular.module('jsxgraph', []).factory('Canvas', function(){
 	        }
 	        return undefined;
 		}
+		//add a point to the board, update the points array
 		this.addPoint = function(coords){
 			var point = board.create('point', [coords.usrCoords[1], coords.usrCoords[2]]);
-			this.points.unshift(point)
-			if(this.points.length == 2){
-				board.create('line',[this.points[0],this.points[1]], {strokeColor:'#00ff00',strokeWidth:2})
-			}
+			this.points.push(point)
 		}
+		//remove the last point
 		this.popPoint = function(){
-			board.removeObject(this.points.shift());
+			board.removeObject(this.points.pop());
 		}
-		this.removePoint = function(point){
-			board.removeObject(point)
+		this.removePoint = function(pointId){
+			board.removeObject(pointId)
 			this.points = _.filter(this.points,function(p){
-				return p.id != point;
-			})			
+				return p.id != pointId;
+			})
 		}
 		this.on = function(event,handler){
 			board.on(event,handler);
+		}
+		this.makeLine = function(){
+			if(this.points.length == 2){
+				return board.create('line',[this.points[0],this.points[1]], {strokeColor:'#00ff00',strokeWidth:2})
+			}
+		}
+		this.prettifyPoints = function(){
+			return _.map(this.points, function(p){
+				return {name: p.name, x: p.initialCoords.usrCoords[1], y: p.initialCoords.usrCoords[2]}
+			})
 		}
 	}
 })
--- public/javascripts/jsxgraph-directive.js
+++ public/javascripts/jsxgraph-directive.js
@@ -1,39 +1,68 @@
 var app = angular.module('jsxgraph');
 app.directive('jsxGraph',function(Canvas){
 	return {
-		template: "<div id='box' class='jxgbox' style='width:500px; height:500px;'></div>",
+		template: "<div id='box' class='jxgbox' style='width:200px; height:200px;'></div>",
 		restrict: 'A',
 		scope: {
-			boardParams: '=boardParams'
+			boardParams: '=',
+			pointsCallback: '=',
+			maxPoints: '@',
+			mousePtr: '=',
+			scale: '@'
 		},
 		link: function(scope,elem,attr){
-				var domain = scope.boardParams.domain
-				var range = scope.boardParams.range
-				if(domain && range) {
-			    	var canvas = new Canvas(JXG.JSXGraph.initBoard('box', {
-			    		boundingbox: [(0 - domain), range, domain, (0 - range)], 
-			    		axis:true
-			    	}));
-			    	//add mouse click listener to create points
-					canvas.on('up', function(e) {
-						var coords = canvas.getMouseCoords(e)
-				 		var pointCollision = canvas.pointCollision(coords)
-				        if (!pointCollision) {
-				        	if(canvas.points.length == 2){
-				        		canvas.popPoint();
-				        	}
-				        	canvas.addPoint(coords)
-				        }else{
-				        	canvas.removePoint(pointCollision)
-				        }
-				    })
-				}else console.error("domain and/or range unspecified");
+			var domain = scope.boardParams.domain
+			var range = scope.boardParams.range
+			if(domain && range) {
+		    	var canvas = new Canvas(JXG.JSXGraph.initBoard('box', {
+		    		boundingbox: [(0 - domain), range, domain, (0 - range)], 
+		    		axis:true
+		    	}));
+		    	//add mouse click listener to create points
+				canvas.on('up', function(e) {
+					var coords = canvas.getMouseCoords(e)
+			 		var pointCollision = canvas.pointCollision(coords)
+			        if (!pointCollision) {
+			        	if(canvas.points.length == scope.maxPoints){
+			        		canvas.popPoint();
+			        	}
+			        	canvas.addPoint(coords)
+			        	if(canvas.points.length == 2){
+			        		var line = canvas.makeLine();
+						}
+			        }else{
+			        	canvas.removePoint(pointCollision)
+			        }
+			        scope.pointsCallback(canvas.prettifyPoints())
+			    })
+			    canvas.on('move', function(e){
+			    	var coords = canvas.getMouseCoords(e)
+			    	scope.mousePtr = {x: coords.usrCoords[1], y: coords.usrCoords[2]}
+			    })
+			}else console.error("domain and/or range unspecified");
 		}
 	}
 });
-app.controller('jsxGraphController', ['$scope',function($scope){
+app.controller('jsxGraphController', ['$scope','$timeout',function($scope,$timeout){
 	$scope.boardParams = {
 		domain: 10,
 		range: 10
+	}
+	$scope.equation = "y = mx + b";
+	$scope.points = {A: {x: "", y: ""}, B: {x: "", y: ""}}
+	$scope.updatePoints = function(points){
+		_.each(points,function(p){
+			$scope.points[p.name] = {x: p.x, y: p.y}
+		})
+		if(points.length == 2){
+			var slope = (points[0].y - points[1].y) / (points[0].x - points[1].x)
+			var yintercept = points[0].y / (points[0].x * slope)
+			$scope.equation = "y = "+slope+"x + "+yintercept;
+		}
+	}
+	$timeout(redraw, 500)
+	function redraw(){
+		$scope.$digest()
+		$timeout(redraw, 500)
 	}
 }]);
